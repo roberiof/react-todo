4053542b4e564970c61b8c07ae9e4ba4de4998da
Robério <antonioroberiofilho8@gmail.com>
2023-02-16 08:20:39 -0300

    adding json server

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Form from './components/Form'
 import Tasks from './components/Tasks'
 
-const api = 'http://localhost:3000/todos'
+const api = 'https://my-json-server.typicode.com/roberiof/react-todo/todos'
 
 function App() {
   const [ todos , setTodos ] = useState( [] )
